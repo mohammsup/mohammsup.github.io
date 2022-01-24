c5db0669f4f09bf0d468ed64f49ca22b72e738ee
Mohammad Bin Yousuf <mohammadby@gmail.com>
2022-01-25 04:02:09 +0800

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,7 +14,9 @@ const config = {
 
 	kit: {
 		adapter: adapter(),
-
+		paths: {
+			base: dev ? "" : ""
+		},
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
